a4240915ad5be9122044431f20877c41a325f129
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-02-21 17:12:18 +0000

    Improved doc

--- 20dataneo4jrest/src/main/java/basic/repo/ThingRepository.java
+++ 20dataneo4jrest/src/main/java/basic/repo/ThingRepository.java
@@ -5,6 +5,16 @@ import org.springframework.stereotype.Repository;
 
 import basic.model.Thing;
 
+/**
+ * Spring will automagically create instances of repository interfaces
+ * as long as they are annotated.
+ * 
+ * No point creating edge repository, since edges are persisted according 
+ * to the nodes they are attached to.
+ * 
+ * @author Adam
+ *
+ */
 @Repository
 public interface ThingRepository extends GraphRepository<Thing> {
 	
